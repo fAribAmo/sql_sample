58b8964b665feb1cfa72ebbd8214c08a467ce120
Fariba Mohammadi <faribamo@kth.se>
2023-11-23 23:44:26 +0100

    Add files via upload

---
+++ generatedata.sql
@@ -0,0 +1,40 @@
+INSERT INTO instroment (instrument_id,,brand,quantity,type)
+VALUES
+  ('346','Sociis Incorporated',1,'individual'),
+  ('871','Vestibulum Ante Ltd',2,'group'),
+  ('553','Fusce Mi Incorporated',5,'group'),
+  ('655','Nec Ligula Ltd',9,'ensembles'),
+  ('524','Pellentesque Tincidunt Tempus Corp.',6,'individual'),
+  ('790','Urna Justo Corporation',5,'individual'),
+  ('856','Arcu Foundation',9,'ensambles'),
+  ('264','Lectus Sit Foundation',4,'ensambles'),
+  ('381','Natoque Incorporated',8,'group'),
+  ('253','Amet Ante Vivamus Institute',7,'group');
+
+
+INSERT INTO person (name,PK,person_number,address,postalZip)
+VALUES
+  ('Jamal Lester',1339,8,'Ap #911-9247 Enim Street','18164-80251'),
+  ('Oprah Gardner',5303,9,'P.O. Box 518, 2007 Elit, Rd.','57690'),
+  ('William Stephenson',1235,0,'254-1120 Sapien, Ave','6421'),
+  ('Lionel Craft',9487,1,'Ap #290-9827 Vulputate Rd.','36733'),
+  ('Armand Becker',7404,8,'P.O. Box 765, 1335 At, Av.','71062'),
+  ('Zachery Walls',5139,6,'4933 Erat, Rd.','4886'),
+  ('Vivian Knox',6409,6,'709-6708 Tempor Avenue','5428-2873'),
+  ('Roanna Greene',5779,3,'P.O. Box 587, 4763 Cubilia Rd.','846563'),
+  ('Noah Warner',6781,3,'Ap #280-8611 Fermentum Rd.','6630'),
+  ('Holmes Rollins',7316,9,'P.O. Box 831, 8170 Scelerisque Av.','263603');
+
+INSERT INTO person_phon (person_id,phone_id,PK)
+VALUES
+  (10,'(224) 866-2489','347'),
+  (4,'(365) 763-7675','711'),
+  (7,'1-442-913-9633','513'),
+  (8,'(392) 313-7921','153'),
+  (3,'1-927-157-0489','381'),
+  (3,'(600) 463-0828','194'),
+  (3,'1-755-513-5531','204'),
+  (4,'1-562-828-5254','585'),
+  (9,'1-216-551-1965','959'),
+  (9,'1-677-621-2723','993');
+
